0568630244a477a289c4dd3d6685006193df5f2e
unknown <zivzango@gmail.com>
2021-02-21 02:11:10 +0200

    changed to convert/files generic, added save wav

--- files_utils.py
+++ files_utils.py
@@ -2,24 +2,37 @@ import os
 import scipy.io
 import numpy
 import soundfile
+import librosa
 
-INT2FLOAT_CONST = 32768.0
+from config import INT2FLOAT_CONST
 
 
-def get_mat_files(dir_path, fs, convert_to_wav=False):
+def get_files(dir_path, extension):
+    """
+    list all the files with wav extension in `dir_path`
+    """
     files = []
     for file in os.listdir(dir_path):
-        if file.endswith(".mat"):
-            noise_name = file.split('.')[0]
-            if convert_to_wav:
-                _convert_mat_to_wav(f"{dir_path}/{file}", noise_name, fs)
+        if file.endswith(f".{extension}"):
             files.append(file)
     return files
 
 
-def _convert_mat_to_wav(mat_file, name, fs):
-    mat_data = scipy.io.loadmat(mat_file)
+def get_wav_data(file, fs):
+    data, _ = librosa.core.load(file, sr=fs)
+    return data
+
+
+def convert_mat_to_wav(file, path_destination, fs):
+    if not file.endswith(".mat"):
+        raise NameError("File needs to be a .mat file")
+    name = file.split('.')[0]
+    mat_data = scipy.io.loadmat(file)
     wav_data = mat_data[name]
     if wav_data.dtype.name == 'int16':
         wav_data = wav_data.astype(numpy.float64, order='C') / INT2FLOAT_CONST
-    soundfile.write(f"./noises/{name}.wav", wav_data, samplerate=fs)
+    soundfile.write(f"{path_destination}{name}.wav", wav_data, samplerate=fs)
+
+
+def save_data_to_wav(data, name, fs=22050, path=os.path.dirname(os.path.abspath(__file__))):
+    soundfile.write(f"{path}\\{name}.wav", data, fs)
