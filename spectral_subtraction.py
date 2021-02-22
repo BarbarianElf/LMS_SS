b2d1899bf23389ac0e3971d83574a9a678b894d4
unknown <zivzango@gmail.com>
2021-02-22 18:10:58 +0200

    changed class methods to private

--- spectral_subtraction.py
+++ spectral_subtraction.py
@@ -8,20 +8,19 @@ import librosa
 class SpectralSubtraction:
 
     def __init__(self, noisy_signal, noise):
-        self._stft(noisy_signal, noise)
+        self.__stft(noisy_signal, noise)
         self.exp_angle = numpy.exp(1.0j * numpy.angle(self.x))
         self.x = numpy.abs(self.x)
         self.d = numpy.mean(numpy.abs(self.d), axis=1)
 
-    def _stft(self, s, n):
+    def __stft(self, s, n):
         self.x = librosa.stft(s)
         self.d = librosa.stft(n)
 
-    def _istft(self):
+    def __istft(self):
         return librosa.istft(self.x)
 
     def calculate(self, alpha=4, beta=0.001):
-        # self.x = self.x - self.d.reshape((self.d.shape[0], 1))
         for i in range(self.x.shape[1]):
             for k in range(self.x.shape[0]):
                 if numpy.square(self.x[k][i]) >= (alpha * self.d[k]):
@@ -29,4 +28,4 @@ class SpectralSubtraction:
                 else:
                     self.x[k][i] = numpy.sqrt(beta * self.d[k])
         self.x = self.x.astype(numpy.complex128) * self.exp_angle
-        return self._istft()
+        return self.__istft()
